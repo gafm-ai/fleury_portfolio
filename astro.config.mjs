023a50896370115e468c752d03315868bb8d4944
gafm-ai <fleurygnonlonfoun@gmail.com>
2024-08-25 12:52:17 +0100

    chore: Update Astro configuration and GitHub Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://gafm-ai.github.io',
+    //base: '/fleury_portfolio',
+});
